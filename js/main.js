232da2161b25eeca6613c2dee485ad1acacb7856
“Erika <foglzerika@gmail.com>
2017-04-11 12:29:09 -0500

    Agregando cancha tenis y fut

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var botonFutbool=document.getElementById('botonFutbool');
 botonFutbool.addEventListener("click",canchaFut);
+var botonBasquet= document.getElementById("btnBasquet");
+botonBasquet.addEventListener("click",canchaBasquet);
+
 
 
 function canchaFut(){
@@ -7,8 +10,8 @@ var cancha= document.getElementById("soccer");
 cancha.style.display="block";
 var canchaTenis= document.getElementById("tenis");
 canchaTenis.style.display="none";
-
-
-
-
+}
+function canchaBasquet(){
+var cancha= document.getElementById("canchaBasket");
+cancha.style.display="none";
 }
